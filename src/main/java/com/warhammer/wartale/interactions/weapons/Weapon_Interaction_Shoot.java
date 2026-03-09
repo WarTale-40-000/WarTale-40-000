feb59151612b6794154d48dcea7c790fbe48f939
Johny <johnydu10@pm.me>
2026-03-09 21:39:02 +0100

    Simplify weapon shooting interaction logic by replacing custom ammo handling with metadata-based implementation.

--- src/main/java/com/warhammer/wartale/interactions/weapons/Weapon_Interaction_Shoot.java
+++ src/main/java/com/warhammer/wartale/interactions/weapons/Weapon_Interaction_Shoot.java
@@ -1,27 +1,20 @@
 package com.warhammer.wartale.interactions.weapons;
 
+import com.hypixel.hytale.codec.Codec;
 import com.hypixel.hytale.codec.builder.BuilderCodec;
 import com.hypixel.hytale.component.CommandBuffer;
 import com.hypixel.hytale.component.Ref;
 import com.hypixel.hytale.logger.HytaleLogger;
 import com.hypixel.hytale.protocol.InteractionState;
 import com.hypixel.hytale.protocol.InteractionType;
-import com.hypixel.hytale.server.core.Message;
-import com.hypixel.hytale.server.core.asset.type.item.config.Item;
 import com.hypixel.hytale.server.core.entity.InteractionContext;
 import com.hypixel.hytale.server.core.entity.entities.Player;
 import com.hypixel.hytale.server.core.inventory.ItemStack;
 import com.hypixel.hytale.server.core.modules.interaction.interaction.CooldownHandler;
 import com.hypixel.hytale.server.core.modules.interaction.interaction.config.SimpleInstantInteraction;
 import com.hypixel.hytale.server.core.universe.world.storage.EntityStore;
-import com.warhammer.wartale.WartalePlugin;
-import com.warhammer.wartale.components.Weapon_Data;
-import com.warhammer.wartale.config.WeaponConfig;
-import com.warhammer.wartale.core.ServiceRegistry;
-import com.warhammer.wartale.types.WarhammerWeaponMetadata;
 
 import javax.annotation.Nonnull;
-import java.util.Map;
 
 public class Weapon_Interaction_Shoot extends SimpleInstantInteraction {
     public static final BuilderCodec<Weapon_Interaction_Shoot> CODEC = BuilderCodec.builder(
@@ -58,47 +51,17 @@ public class Weapon_Interaction_Shoot extends SimpleInstantInteraction {
             return;
         }
 
-        Item item = itemStack.getItem();
-        String weaponID = item.getId();
+        String weaponID = itemStack.getItem().getId();
+        Integer currentAmmoAmount = itemStack.getFromMetadataOrNull("ammo", Codec.INTEGER);
 
-        // Initialize ammunition component
-        Weapon_Data weaponData = new Weapon_Data();
-        if (commandBuffer.getComponent(ref, WartalePlugin.WEAPON_DATA) != null) {
-            // here we implement logic that updates the component
-            weaponData = commandBuffer.getComponent(ref, WartalePlugin.WEAPON_DATA);
-        } else {
-            // putComponent allows you to insert declared objects
-            commandBuffer.putComponent(ref, WartalePlugin.WEAPON_DATA, weaponData);
-        }
-
-        assert weaponData != null;
-        Map<String, Integer> currentAmmoMap = weaponData.getCurrentAmmo();
-        WeaponConfig weaponConfig = ServiceRegistry.get(WeaponConfig.class);
-        Map<String, WarhammerWeaponMetadata> metadata = weaponConfig.getWeapons();
-        WarhammerWeaponMetadata weaponMetadata = metadata.get(weaponID);
-        if (weaponMetadata == null) {
-            player.sendMessage(Message.raw("The weapon " + weaponID + " is not a registered weapon."));
-            LOGGER.atInfo().log("The weapon " + weaponID + " is not a registered weapon.");
+        if (currentAmmoAmount == null || currentAmmoAmount <= 0) {
             interactionContext.getState().state = InteractionState.Failed;
-        } else {
-            Integer currentAmmoValue = currentAmmoMap.get(weaponID);
-            weaponData.setCurrentWeaponId(weaponID);
-
-            if (currentAmmoValue == null || currentAmmoValue <= 0) {
-                player.sendMessage(Message.raw("Mag Empty! Reloading weapon."));
-                LOGGER.atInfo().log("Mag Empty for weapon: " + weaponID);
-                interactionContext.getState().state = InteractionState.Failed;
-            } else {
-                // Decrease ammunition count
-                currentAmmoMap.put(weaponID, currentAmmoValue - 1);
-                commandBuffer.putComponent(ref, WartalePlugin.WEAPON_DATA, weaponData);
-                // weaponData.setCurrentAmmo(currentAmmo);
-                String message = "Fired weapon: " + weaponID + ". Magazine: " + (currentAmmoValue - 1) + "/"
-                        + weaponMetadata.getMaxAmmo();
-                player.sendMessage(Message.raw(message));
-                LOGGER.atInfo().log(message);
-            }
+            LOGGER.atInfo().log("Mag empty for weapon: " + weaponID);
+            return;
         }
+
+        byte slot = player.getInventory().getActiveHotbarSlot();
+        player.getInventory().getHotbar().setItemStackForSlot(slot, itemStack.withMetadata("ammo", Codec.INTEGER, currentAmmoAmount - 1));
     }
 
 }
