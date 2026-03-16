3c436d404967da0f7dcf1df8c51f39de1d80ee63
Marcel T.O <marcel@turobin-ort.at>
2026-03-16 11:46:19 +0100

    feat: add Weapon Icon to Ammo Text

--- src/main/java/com/warhammer/wartale/gui/WartaleHUD.java
+++ src/main/java/com/warhammer/wartale/gui/WartaleHUD.java
@@ -17,11 +17,20 @@ public class WartaleHUD extends CustomUIHud {
         ui.append("HUD/WartaleHUD.ui");
     }
 
-    public void setAmmoSection(boolean visible, String ammoText) {
+    public void setAmmoSection(boolean visible, String ammoText, String weaponId, boolean shouldReload) {
         UICommandBuilder patch = new UICommandBuilder();
         patch.set("#Content.Visible", visible);
+
         if (visible) {
+            patch.set("#WeaponIcon.Background", "Icons/Weapons/" + weaponId + ".png");
             patch.set("#CurrentAmmo.Text", ammoText);
+
+            // Recolors Text if Ammo low
+            if (shouldReload) {
+                patch.set("#CurrentAmmo.Style.TextColor", "#ff0000");
+            } else  {
+                patch.set("#CurrentAmmo.Style.TextColor", "#ffffff");
+            }
         }
         update(false, patch);
     }
