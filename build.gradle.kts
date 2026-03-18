da16929ff90b711c28da726f81487ef5cc0396df
Johny <johnydu10@pm.me>
2026-03-18 22:12:59 +0100

    Add GitHub Actions workflow for CI with build and artifact upload

--- build.gradle.kts
+++ build.gradle.kts
@@ -18,7 +18,7 @@ hytale {
         Name = "Wartale"
         Main = "com.warhammer.wartale.WartalePlugin"
         Version = manifestVersion
-        ServerVersion = "2026.03.19-c0865d17c"
+        ServerVersion = "pre-release/+"
         Dependencies = mapOf("Hytale:EntityModule" to "*")
     }
 }
