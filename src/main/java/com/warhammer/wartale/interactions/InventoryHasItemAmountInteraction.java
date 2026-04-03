131076e9cbc8a46189471011d5e32110ac74cff2
Johny <johnydu10@pm.me>
2026-04-03 22:41:09 +0200

    Refactor reload system: replace `ValidateReloadInteraction` and `ReloadInteraction` with root-level reload definitions. Migrate weapon files to new interaction structure and remove unused properties.

---
+++ src/main/java/com/warhammer/wartale/interactions/InventoryHasItemAmountInteraction.java
@@ -0,0 +1,81 @@
+package com.warhammer.wartale.interactions;
+
+import com.hypixel.hytale.codec.Codec;
+import com.hypixel.hytale.codec.KeyedCodec;
+import com.hypixel.hytale.codec.builder.BuilderCodec;
+import com.hypixel.hytale.codec.validation.Validators;
+import com.hypixel.hytale.component.CommandBuffer;
+import com.hypixel.hytale.component.Ref;
+import com.hypixel.hytale.logger.HytaleLogger;
+import com.hypixel.hytale.protocol.InteractionState;
+import com.hypixel.hytale.protocol.InteractionType;
+import com.hypixel.hytale.protocol.WaitForDataFrom;
+import com.hypixel.hytale.server.core.asset.type.item.config.Item;
+import com.hypixel.hytale.server.core.entity.InteractionContext;
+import com.hypixel.hytale.server.core.entity.entities.Player;
+import com.hypixel.hytale.server.core.inventory.InventoryComponent;
+import com.hypixel.hytale.server.core.inventory.container.CombinedItemContainer;
+import com.hypixel.hytale.server.core.modules.interaction.interaction.CooldownHandler;
+import com.hypixel.hytale.server.core.modules.interaction.interaction.config.SimpleInstantInteraction;
+import com.hypixel.hytale.server.core.universe.world.storage.EntityStore;
+
+import javax.annotation.Nonnull;
+
+public class InventoryHasItemAmountInteraction extends SimpleInstantInteraction {
+    public static final BuilderCodec<InventoryHasItemAmountInteraction> CODEC = BuilderCodec.builder(
+                    InventoryHasItemAmountInteraction.class,
+                    InventoryHasItemAmountInteraction::new,
+                    SimpleInstantInteraction.CODEC)
+            .appendInherited(new KeyedCodec<>("ItemId", Codec.STRING, true),
+                             (obj, val) -> obj.itemId = val,
+                             obj -> obj.itemId,
+                             (obj, p) -> obj.itemId = p.itemId)
+            .addValidator(Validators.nonNull())
+            .addValidatorLate(() -> Item.VALIDATOR_CACHE.getValidator().late())
+            .add()
+            .appendInherited(new KeyedCodec<>("Amount", Codec.INTEGER, true),
+                             (obj, val) -> obj.amount = val,
+                             obj -> obj.amount,
+                             (obj, p) -> obj.amount = p.amount)
+            .addValidator(Validators.greaterThan(0))
+            .add()
+            .build();
+    public static final HytaleLogger LOGGER = HytaleLogger.forEnclosingClass();
+    private String itemId;
+    private int amount;
+
+    @Nonnull
+    @Override
+    public WaitForDataFrom getWaitForDataFrom() {
+        return WaitForDataFrom.Server;
+    }
+
+    @Override
+    protected void firstRun(@Nonnull InteractionType interactionType,
+                            @Nonnull InteractionContext interactionContext,
+                            @Nonnull CooldownHandler cooldownHandler) {
+        CommandBuffer<EntityStore> commandBuffer = interactionContext.getCommandBuffer();
+        if (commandBuffer == null) {
+            interactionContext.getState().state = InteractionState.Failed;
+            LOGGER.atWarning().log("CommandBuffer is null");
+            return;
+        }
+
+        Ref<EntityStore> ref = interactionContext.getEntity();
+        Player player = commandBuffer.getComponent(ref, Player.getComponentType());
+        if (player == null) {
+            interactionContext.getState().state = InteractionState.Failed;
+            LOGGER.atWarning().log("Player is null");
+            return;
+        }
+
+        CombinedItemContainer inventory = InventoryComponent.getCombined(commandBuffer,
+                                                                         ref,
+                                                                         InventoryComponent.HOTBAR_FIRST);
+        int itemCount = inventory.countItemStacks(stack -> itemId.equals(stack.getItemId()));
+
+        if (itemCount < amount) {
+            interactionContext.getState().state = InteractionState.Failed;
+        }
+    }
+}
---
+++ src/main/java/com/warhammer/wartale/interactions/weapons/LoadMagazineInteraction.java
@@ -0,0 +1,60 @@
+package com.warhammer.wartale.interactions.weapons;
+
+import com.hypixel.hytale.codec.Codec;
+import com.hypixel.hytale.codec.KeyedCodec;
+import com.hypixel.hytale.codec.builder.BuilderCodec;
+import com.hypixel.hytale.logger.HytaleLogger;
+import com.hypixel.hytale.protocol.InteractionState;
+import com.hypixel.hytale.protocol.InteractionType;
+import com.hypixel.hytale.protocol.WaitForDataFrom;
+import com.hypixel.hytale.server.core.entity.InteractionContext;
+import com.hypixel.hytale.server.core.inventory.ItemStack;
+import com.hypixel.hytale.server.core.modules.interaction.interaction.CooldownHandler;
+import com.hypixel.hytale.server.core.modules.interaction.interaction.config.SimpleInstantInteraction;
+import com.warhammer.wartale.items.WeaponMetadataKey;
+
+import javax.annotation.Nonnull;
+
+public class LoadMagazineInteraction extends SimpleInstantInteraction {
+    private int reloadAmount;
+
+    public static final BuilderCodec<LoadMagazineInteraction> CODEC = BuilderCodec
+            .builder(LoadMagazineInteraction.class, LoadMagazineInteraction::new, SimpleInstantInteraction.CODEC)
+            .appendInherited(
+                    new KeyedCodec<>("ReloadAmount", Codec.INTEGER, true), (obj, val) -> obj.reloadAmount = val, obj -> obj.reloadAmount, (obj, p) -> obj.reloadAmount = p.reloadAmount
+            )
+            .add()
+            .build();
+
+    public static final HytaleLogger LOGGER = HytaleLogger.forEnclosingClass();
+
+    @Nonnull
+    @Override
+    public WaitForDataFrom getWaitForDataFrom() {
+        return WaitForDataFrom.Server;
+    }
+
+    @Override
+    protected void firstRun(@Nonnull InteractionType interactionType, @Nonnull InteractionContext interactionContext, @Nonnull CooldownHandler cooldownHandler) {
+        LOGGER.atInfo().log("LoadMagazineInteraction firstRun");
+        ItemStack itemStack = interactionContext.getHeldItem();
+        if (itemStack == null) {
+            interactionContext.getState().state = InteractionState.Failed;
+            LOGGER.atInfo().log("ItemStack is null");
+            return;
+        }
+
+        if (interactionContext.getHeldItemContainer() == null) {
+            interactionContext.getState().state = InteractionState.Failed;
+            LOGGER.atInfo().log("HeldItemContainer is null");
+            return;
+        }
+
+        ItemStack newItemStack = itemStack
+                .withMetadata(WeaponMetadataKey.CURRENT_AMMO.key(), Codec.INTEGER, reloadAmount)
+                .withMetadata(WeaponMetadataKey.MAG_SIZE.key(), Codec.INTEGER, reloadAmount);
+
+        interactionContext.getHeldItemContainer().setItemStackForSlot(interactionContext.getHeldItemSlot(), newItemStack);
+        interactionContext.setHeldItem(newItemStack);
+    }
+}
